6bab885d37acd306d3146e6d824dd5309ab24c5f
davengahu007 <davengahu007@gmail.com>
2023-06-22 20:54:09 +0300

    Auth_1

--- app/Helper/UserService.php
+++ app/Helper/UserService.php
@@ -2,6 +2,8 @@
 
 namespace App\Helper;
 
+
+use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Validation\Rules\Password;
@@ -9,17 +11,19 @@ use Illuminate\Validation\Rules\Password;
 class UserService{
     public $email, $password;
 
-    public function __construct($email, $password){
+    public function __construct($email, $password)
+    {
         $this->email = $email;
         $this->password = $password;
     }
 
     public function validateInput()
     {
-        $validator = Validator::make(['email' => $this->email, 'password'=>$this->password],
+        $validator = Validator::make(
+            ['email' => $this->email, 'password'=>$this->password],
         [
             'email' => ['required', 'email:rfc,dns', 'unique:users'],
-            'password' => ['required', 'string' , Password::min(8)]
+            'password' => ['required', 'string' , Password::min(8)->letters()->numbers()->mixedCase()]
         ]
         );
 
@@ -41,8 +45,16 @@ class UserService{
             return $validate;
         }
         else{
-            $user = User::create(['email'=>$this->email, 'password'=>Hash::make($this->password)]);
-            $token = $user ->createToken($deviceName)->plainText;
+            $user = User::create([
+                'email'=>$this->email, 
+                'password'=>Hash::make($this->password)]);
+
+            // $token = $user ->createToken($deviceName)->plainTextToken;     
+            $token = $deviceName
+            ? $user->createToken($deviceName)->plainTextToken
+            : $user->createToken('')->plainTextToken;
+
+            return ['status' => true, 'token' => $token, 'user' => $user];
         }
     }
 }
